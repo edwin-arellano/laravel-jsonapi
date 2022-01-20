dc2dbbbb22bba59a8aa1bd36030171a60c04d1dc
Edwin Arellano <edwin.i.arellano@gmail.com>
2022-01-19 22:24:45 -0600

    Fetch all articles

--- app/Http/Controllers/Api/V1/ArticleController.php
+++ app/Http/Controllers/Api/V1/ArticleController.php
@@ -6,9 +6,15 @@ use App\Models\Article;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ArticleResource;
+use App\Http\Resources\ArticleCollection;
 
 class ArticleController extends Controller
 {
+    public function index(): ArticleCollection
+    {
+        return ArticleCollection::make(Article::all());
+    }
+
     public function show(Article $article): ArticleResource
     {
         return ArticleResource::make($article);
