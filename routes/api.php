538b5aa60651ba965b3a07ef254f9321fba41197
Edwin Arellano <edwin.i.arellano@gmail.com>
2022-01-20 17:32:14 -0600

    Create articles

--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,7 @@ Route::prefix('v1')->group(function() {
 
     Route::get('articles/{article}', [ArticleController::class, 'show'])
         ->name('api.v1.articles.show');
+    
+    Route::post('articles', [ArticleController::class, 'create'])
+        ->name('api.v1.articles.create');
 });
