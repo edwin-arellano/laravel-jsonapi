26146e35e5477fb778bf987dc84b8b036973ec00
Edwin Arellano <edwin.i.arellano@gmail.com>
2022-01-21 20:01:17 -0600

    Macro for Query Builder

--- app/Http/Controllers/Api/V1/ArticleController.php
+++ app/Http/Controllers/Api/V1/ArticleController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Article;
 use Illuminate\Support\Str;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ArticleResource;
@@ -13,25 +12,9 @@ use App\Http\Resources\ArticleCollection;
 
 class ArticleController extends Controller
 {
-    public function index(Request $request): ArticleCollection
+    public function index(): ArticleCollection
     {
-        $articles = Article::query();
-
-        if ($request->filled('sort')) {
-            $sortFields = explode(',', $request->input('sort'));
-
-            $allowedSorts = ['title', 'content'];
-
-            foreach ($sortFields as $sortField) {
-                $sortDirection = Str::of($sortField)->startsWith('-') ? 'desc' : 'asc';
-
-                $sortField = ltrim($sortField, '-');
-
-                abort_unless(in_array($sortField, $allowedSorts), 400);
-
-                $articles->orderBy($sortField, $sortDirection);
-            }
-        }
+        $articles = Article::allowedSorts(['title', 'content']);
 
         return ArticleCollection::make($articles->get());
     }
