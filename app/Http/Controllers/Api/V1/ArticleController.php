bb11ff433c0aaa492c5d5f835691c598bb711f41
Edwin Arellano <edwin.i.arellano@gmail.com>
2022-01-21 19:15:32 -0600

    Sort Articles single field

--- app/Http/Controllers/Api/V1/ArticleController.php
+++ app/Http/Controllers/Api/V1/ArticleController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Article;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ArticleResource;
@@ -11,9 +13,17 @@ use App\Http\Resources\ArticleCollection;
 
 class ArticleController extends Controller
 {
-    public function index(): ArticleCollection
+    public function index(Request $request): ArticleCollection
     {
-        return ArticleCollection::make(Article::all());
+        $sortField = $request->input('sort');
+
+        $sortDirection = Str::of($sortField)->startsWith('-') ? 'desc' : 'asc';
+
+        $sortField = ltrim($sortField, '-');
+
+        $articles = Article::orderBy($sortField, $sortDirection)->get();
+
+        return ArticleCollection::make($articles);
     }
 
     public function show(Article $article): ArticleResource
